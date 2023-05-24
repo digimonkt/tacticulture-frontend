12808c3548e7d69fbdaf675aaa2d4cb9a77e703d
alisarfaraj <sarfaraj.ali@digimonk.in>
2023-05-24 18:49:32 +0530

    fix

--- src/component/setup-profile/instructor/step2.tsx
+++ src/component/setup-profile/instructor/step2.tsx
@@ -2,7 +2,7 @@ import { SVG } from "@/assets/svg";
 import { Checkbox, Col, Row } from "antd";
 import React, { Ref, forwardRef, useImperativeHandle } from "react";
 import styles from "../../../pages/setup-profile/profile.module.css";
-import { TimepickerInput } from "@/component/input";
+import { LabeledInput } from "@/component/input";
 import { useDispatch } from "react-redux";
 import { useFormik } from "formik";
 import { setPreLoader } from "@/redux/reducers/preLoader";
@@ -123,31 +123,32 @@ const Step2 = forwardRef(function Step2(props, ref: Ref<InstructorStepTwoRef>) {
         <Col md={12} className="ps-4 pe-2">
           <div className={`${styles.Hours}`}>
             <label>Available Hours</label>
-            <TimepickerInput
+            {/* <TimepickerInput
               className="timePickers"
               suffixIcon={<SVG.DownChevron width="16px" />}
-              {...formik.getFieldProps("availableFrom")}
-            />
-            {/* <LabeledInput
+              onChange={(vl) => console.log("e -- ", vl)}
+              // {...formik.getFieldProps("availableFrom")}
+            /> */}
+            <LabeledInput
               type="time"
               className="form-control"
               {...formik.getFieldProps("availableFrom")}
-            /> */}
+            />
           </div>
         </Col>
         <Col md={12} className="pe-4 ps-2">
           <div className={`${styles.Hours}`}>
             <label className="mb-4"></label>
-            <TimepickerInput
+            {/* <TimepickerInput
               suffixIcon={<SVG.DownChevron width="16px" />}
               className="timePickers"
               {...formik.getFieldProps("availableTo")}
-            />
-            {/* <LabeledInput
+            /> */}
+            <LabeledInput
               type="time"
               className="form-control"
               {...formik.getFieldProps("availableTo")}
-            /> */}
+            />
           </div>
         </Col>
       </Row>
